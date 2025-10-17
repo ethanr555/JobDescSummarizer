8862fda089bfe841a2a8598747a48f403ac0297c
Ethan <ethan1randolph@gmail.com>
2025-10-16 20:26:48 -0700

    Attempting to better secure the docker development environment. It is recommended that Docker does not run in root, so for the standalone.sh script, it will not run in root anymore, but instead build the image to use host user uid to run everything. This also fixes an issue where if running build and test commands, the generated files would have the owner as root and permissions set for root only. Act though needs root, as it is using the host docker daemon, and the docker group on host does not have the docker user added. docker-act-exec.sh will build the image and run in root, but run act instead of bash.

--- .devcontainer/docker-act-exec.sh
+++ .devcontainer/docker-act-exec.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 IMAGENAME=job-desc-summarizer-dev-env
@@ -8,4 +9,4 @@ USERUID=$(id -u $(who | awk '{print $1}'))
 docker build -f $(dirname "$0")/standalone.Dockerfile -qt $IMAGENAME .
 IMAGE_ACT=$(docker build --build-arg IN_IMAGE=$IMAGENAME --build-arg USERUID=$USERUID  -f $(dirname "$0")/standalone_act.Dockerfile -qt $IMAGENAME_ACT . )
 
-docker run --user $USERUID:$USERUID -itv $(dirname "$0")/../:/project -v /var/run/docker.sock:/var/run/docker.sock $IMAGE_ACT
+docker run -itv $(dirname "$0")/../:/project -v /var/run/docker.sock:/var/run/docker.sock $IMAGE_ACT act --artifact-server-path /tmp/artifacts
